18fb8e0d276f738bd1b41801629b32a3d7e4a189
Aayush Bhutani <abhutan1@ghc31.ghc.andrew.cmu.edu>
2018-04-17 22:56:12 -0400

    graph file parsing

--- flow-seq.cpp
+++ flow-seq.cpp
@@ -1,19 +1,78 @@
 #include <iostream> 
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+#include <string>
 
 #include "graph-seq.h"
 
 using namespace std;
  
-int main(int argc, char** argv) {
-    Graph graph(6);
-    graph.addEdges(0,1,11);
-    graph.addEdges(0,2,12);
-    graph.addEdges(2,1,1);
-    graph.addEdges(1,3,12);
-    graph.addEdges(2,4,11);
-    graph.addEdges(4,3,7);
-    graph.addEdges(3,5,19);
-    graph.addEdges(4,5,4);
-    cout << graph.maxFlow(0,5)<< endl;
-    return 0;
+int main(int argc, char* argv[]) {
+    if (argc < 3) { 
+        std::cout << "Usage: -g <graph.txt>"<< endl; 
+        exit(0);
+    }
+    else{
+        char* FILE = NULL;
+        string flag = "-g";
+        if (argv[1] == flag){
+            FILE = argv[2];
+        }
+        if (FILE == NULL){
+            std::cout << "Please enter a valid graph.txt file>" << endl; 
+            std::cout << "Usage: -g <graph.txt>" << endl; 
+            exit(0);
+        }
+        else{
+            ifstream file(FILE);
+            string data;
+            int num_vertices, s, t, max_flow_expected, max_flow;
+            num_vertices = 0;
+            s = 0;
+            t = 0;
+            max_flow_expected = 0;
+            getline(file, data);
+            num_vertices = std::stoi(data);
+            if (num_vertices == 0){
+                std::cout << "Error: vertices = 0" << endl;
+                exit(0);
+            }
+            Graph graph(num_vertices);
+            while (file && getline(file, data)){
+                std::vector<std::string> array;
+                std::size_t pos = 0, found;
+                while((found = data.find_first_of(' ',pos))!= std::string::npos){
+                    array.push_back(data.substr(pos, found - pos));
+                    pos = found+1;
+                }
+                array.push_back(data.substr(pos));
+                if (array.size() == 0){
+                    continue;
+                }
+                else if (array.size() == 1){
+                    max_flow_expected = std::stoi(array[0]);
+                }
+                else if (array.size() == 2){
+                    s = std::stoi(array[0]);
+                    t = std::stoi(array[1]);
+                }
+                else if (array.size() == 3){
+                    graph.addEdges(std::stoi(array[0]),std::stoi(array[1]),
+                                                        std::stoi(array[2]));
+                }
+            }
+            std::cout << "Testing with input graph file = " << FILE << endl; 
+            max_flow = graph.maxFlow(s,t);
+            if (max_flow == max_flow_expected){
+                std::cout << "Passed" << endl;
+            }
+            else{
+                std::cout << "Failed" << endl; 
+                std::cout << "Flow returned = " << max_flow << " " << 
+                "Flow expected = " << max_flow_expected << endl;
+            }
+            return 0;
+        }
+    }
 }
