d781712df6a9bf7cb1b0b50a306b24252adfa8e6
Aayush Bhutani <abhutan1@ghc31.ghc.andrew.cmu.edu>
2018-04-23 19:04:52 -0400

    Organizing files

---
+++ sequential/graph-seq.h
@@ -0,0 +1,18 @@
+#ifndef GRAPH_SEQ_H
+#define GRAPH_SEQ_H
+
+#include <vector>
+
+class Graph {
+public:
+    Graph(int num_vertices);
+    void addEdges(int u, int v, int w);
+    int maxFlow(int s, int t);
+
+private:
+    bool findPath(int s, int d, std::vector<int>& parentP);
+    int num_vertices;
+    std::vector< std::vector< int > > graph;
+};
+
+#endif
