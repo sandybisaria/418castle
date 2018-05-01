c8f795df1e839f6d9b9a9cd78b4d2191ae8dae8a
Aayush Bhutani <abhutan1@ghc31.ghc.andrew.cmu.edu>
2018-04-30 21:55:31 -0400

    makefile

---
+++ openmp/flow-omp.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+#include <string>
+
+#include "graph-seq.h"
+
+using namespace std;
+ 
+int main(int argc, char* argv[]) {
+    return 0;
+}
--- openmp/graph-seq.cpp
+++ openmp/graph-seq.cpp
@@ -7,7 +7,7 @@
 Graph::Graph(int num_vertices){
     graph.assign(num_vertices, std::vector<int>(num_vertices, 0));
     this->num_vertices = num_vertices;
-} 
+}
 
 void Graph::addEdges(int u, int v, int w){
     graph[u][v] = w;
